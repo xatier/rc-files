a2c49caf8ea42825f7444ca82b699281bbf1e538
xatier <xatierlike@gmail.com>
2015-02-06 01:33:07 +0800

    disable transparency

--- rc.lua
+++ rc.lua
@@ -641,6 +641,6 @@ client.connect_signal("manage", function (c, startup)
     end
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus c.opacity = 1 end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal c.opacity = 0.95 end)
+client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
