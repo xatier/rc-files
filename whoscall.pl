ba11c515e2943ff93aab6a4c3c78c368be44a3bb
xatier <xatierlike@gmail.com>
2015-03-11 14:36:18 +0800

    add a who's call command line tool

---
+++ whoscall.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use 5.014;
+
+my $number = join "", @ARGV;
+
+chomp $number;
+
+$number =~ s/\s+//g;
+$number =~ s/^09/886/;
+$number =~ s/\+886/886/;
+
+say $number;
+
+say "Wrong number: @ARGV" and exit if $number !~ /^\d{12}$/;
+say `w3m -dump http://whoscall.com/number/$number/?country_code=tw | tail -n+12 | head -n-24`;
