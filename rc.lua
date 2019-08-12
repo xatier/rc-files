417387219452c64f5eaa4e95ee7960ee83a7a1bb
xatier <xatierlike@gmail.com>
2019-08-11 18:26:36 -0700

    Sync with upstream

--- rc.lua
+++ rc.lua
@@ -673,28 +673,32 @@ awful.rules.rules = {
         instance = {
           "DTA",  -- Firefox addon DownThemAll.
           "copyq",  -- Includes session name in class.
+          "pinentry",
         },
         class = {
-          "Mplayer",
-          "mpv",
-          "pinetry",
-          "exe",
           "Arandr",
+          "Blueman-manager",
+          "exe",
           "Gpick",
           "Minecraft 1.11.2",
+          "Mplayer",
+          "mpv",
           "Kruler",
           "MessageWin",  -- kalarm.
           "Sxiv",
+          "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
-          "pinentry",
           "veromix",
           "xtightvncviewer"},
 
+        -- Note that the name property shown in xprop might be set slightly after creation of the client
+        -- and the name shown there might not match defined rules here.
         name = {
           "Event Tester",  -- xev.
         },
         role = {
           "AlarmWindow",  -- Thunderbird's calendar.
+          "ConfigManager",  -- Thunderbird's about:config.
           "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
         }
       }, properties = { floating = true }},
