f7ab2b12985a60ac5018ef06eea2f6aed767db9d
xatier <xatierlike@gmail.com>
2021-02-17 05:22:48 -0800

    Make Einstein float!

--- rc.lua
+++ rc.lua
@@ -753,6 +753,10 @@ awful.rules.rules = {
     { rule = { class = "albert" },
       properties = { border_width = 0, skip_taskbar = true } },
 
+    ---- Set Einstein to be floating
+    --{ rule = { class = "einstein" },
+    --  properties = { floating = true } },
+
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
