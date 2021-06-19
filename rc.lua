f9d170471201fea755756c07963dc26e21c0e3a8
xatier <xatierlike@gmail.com>
2021-06-19 17:58:37 -0500

    Update font size for new monitors

--- rc.lua
+++ rc.lua
@@ -50,7 +50,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-beautiful.font = "Inconsolata 12"
+beautiful.font = "Inconsolata 10"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvtc"
